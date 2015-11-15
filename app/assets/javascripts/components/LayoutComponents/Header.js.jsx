16e2da2901ee6f0fd1fc3b3a4427ebee7a54a4a0
acoravos <acoravos@gmail.com>
2015-11-15 00:37:27 -0500

    remove wrench from header

--- app/assets/javascripts/components/LayoutComponents/Header.js.jsx
+++ app/assets/javascripts/components/LayoutComponents/Header.js.jsx
@@ -11,8 +11,8 @@ var Header = React.createClass({
             <FixedMenuTemplate />
             <MobileMenu />
 
-          <div className="ui huge icon header inverted">
-            <a href="/dashboard"> <i className="circular configure icon inverted"></i></a> <span className="header_text">MeToo</span>
+          <div className="ui huge icon header inverted" id="padding50">
+            <a href="/dashboard"> </a> <span className="header_text">MeToo</span>
           </div>
 
           <h2 className="ui header inverted">
